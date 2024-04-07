217e86ade3a459b992ba13c9826198ad6943b06b
omwami <ianalpha@protonmail.com>
2024-04-07 19:56:11 +0300

    add fns to create youtube playlist & insert items

--- app.py
+++ app.py
@@ -8,5 +8,24 @@ my_app = Spo2yt()
 
 @app.route("/")
 def index():
+    """ Index page
+        Returns all your current saved playlists
+    """
     playlists = my_app.get_spotify_playlists()
     return render_template('index.html', playlists=playlists)
+
+@app.route('/playlist/<string:play_id>')
+def goto_spotify_playlist(play_id:str):
+    """ Goes to page for specific playlist """
+    name, tracks = my_app.get_spotify_playlist_tracks(play_id)
+    res = my_app.create_youtube_music_playlist(name, tracks)
+    print(res)
+    return render_template('playlist.html', name=name, tracks=tracks)
+
+@app.route('/create-playlist/')
+def create_youtube_music_playlist(songs):
+    return "Redirecting to youtube playlists"
+    
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
--- spo2yt.py
+++ spo2yt.py
@@ -5,6 +5,7 @@ from base import Base
 class Spo2yt(Base):
     """ Complete class with spotify & youtube music resources """
     def get_spotify_playlists(self):
+        """ Get all your current saved spotify playlists """
         playlists = self.spotify.current_user_playlists().get('items')
         p_info = list()
         for playlist in playlists:
@@ -15,9 +16,11 @@ class Spo2yt(Base):
         return p_info
 
     def get_spotify_playlist_tracks(self, playlist_id: str):
+        """ Get tracks for a spotify playlist """
         if not playlist_id:
             return []
         playlist = self.spotify.playlist(playlist_id)
+        p_name = playlist['name']
         tracks = list()
         for item in playlist['tracks'].get('items'):
             # tracks.append(item)
@@ -25,4 +28,24 @@ class Spo2yt(Base):
                 # for some reason, there exists some empty song items
                 data = {'id': item['track']['id'], 'name': item['track']['name']}
                 tracks.append(data)
-        return tracks
+        return (p_name, tracks)
+
+    def search_song_youtube(self, song_name: str):
+        """ Search for song and return info else return None """
+        if not song_name:
+            return None
+        kwargs = {'q': song_name, 'maxResults': 10}
+        return self.youtube.search().list(part='id,snippet', **kwargs).execute().get('items')[0]
+
+    def create_youtube_music_playlist(self, p_name: str, songs: list):
+        """ Creates a youtube playlist from a list of songs """
+        # part = 'id,snippet,status,localizations,contentDetails'
+        part = 'id,snippet,status,contentDetails'
+        resource = {'kind': "youtube#playlist", "snippet": {'title': p_name, 'defaultLanguage': 'en'}}
+        yt_playlist = self.youtube.playlists().insert(part=part, body=resource).execute()
+        if yt_playlist:
+            for song in songs:
+                insert_data = {'kind': "youtube#playlistItem", 'snippet': {'playlistId': yt_playlist.get('id'), 'resourceId': {'kind': "youtube#video", 'videoId': song.get('id')}}}
+                res = self.youtube.playlistItems().insert(part=part, body=insert_data).execute()
+                print(f"Response after inserting playlist item: {res}")
+        return yt_playlist
